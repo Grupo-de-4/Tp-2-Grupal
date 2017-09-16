f6ecba80930ccb3dd19d9fdc7e13f04ac794c3e8
Maximiliano Rodrigo Di Lorenzo <maximiliano.rdl@gmail.com>
2017-09-15 22:05:21 -0300

    Agregado Horner y comentarios

--- src/matematica/Polinomio.java
+++ src/matematica/Polinomio.java
@@ -22,7 +22,7 @@ public class Polinomio {
 	public double evaluarMSucesivas(double x) {
 		double res = 0;
 		for (int i = this.grado; i >= 0; i--)
-			res += coeficientes[i] * potenciaMSucesivas(x, i);
+			res += coeficientes[i] * potenciaMSucesivas(x, i); //O(n^2)
 
 		return res;
 	}
@@ -30,7 +30,7 @@ public class Polinomio {
 	public double evaluarRecursiva(double x) {
 		double res = 0;
 		for (int i = this.grado; i >= 0; i--)
-			res += coeficientes[i] * potencia(x, i);
+			res += coeficientes[i] * potencia(x, i);		//O(n) por el ciclo, O(n) por la potencia, O(n^2)
 
 		return res;
 	}
@@ -39,57 +39,71 @@ public class Polinomio {
 		double res = 0;
 		if(grado%2==0)
 		for (int i = this.grado; i >= 0; i--)
-			res += coeficientes[i] * (x % 2 == 0 ? potenciaPar(x, i) : potencia(x, i));
-
-		return res;
+			res += coeficientes[i] * (x % 2 == 0 ? potenciaPar(x, i) : potencia(x, i)); //O(n) *(Log[n] + O(n))
+																						//= O(n^2)
+		return res;													
 	}
 
 	public double evaluarProgDinamica(double x) {
 		double res = 0;
 		double []vPot= new double[grado+1];
 		for (int i = this.grado; i >= 0; i--)
-			res += coeficientes[i] * potenciaDinamica(x, i, vPot);
-		return res;
-	}
+			res += coeficientes[i] * potenciaDinamica(x, i, vPot);	//O(n) por el ciclo, pero las potencias 
+		return res;													//solo se calculan una sola vez O(n)
+	}																//asi que seria O(n) + O(n) = O(n)
 	
 	public double evaluarPow(double x) {
 		double res = 0;
 		if(grado%2==0)
-		for (int i = this.grado; i >= 0; i--)
+		for (int i = this.grado; i >= 0; i--)			//O(n) * O(pow)
 			res += coeficientes[i] * Math.pow(x, i);
 
 		return res;
 	}
-	//POTENCIAS
-	public double potenciaMSucesivas(double x, int n)
+	
+	public double evaluarHorner(double x)
 	{
-		double res=x;
-		for(int i=0; i<n; i++)
-			res*=x;
+		double res=0;
+		
+		for(int i = 0; i<=grado; i++)
+		{
+			res = res*x + coeficientes[i];
+		}
+		
 		return res;
 	}
 	
-	public double potencia(double x, int n) {
-		if (n == 0)
-			return 1;
-		return x * potencia(x, n - 1);
+	
+	//POTENCIAS
+	public double potenciaMSucesivas(double x, int n) 	//Complejidad
+	{
+		double res=x;									// 1
+		for(int i=0; i<n; i++)							// n
+			res*=x;											// 1
+		return res;										//TOTAL O(n*1 +1) = O(n)
+	}
+	
+	public double potencia(double x, int n) {			//Complejidad
+		if (n == 0)										// 1
+			return 1;	
+		return x * potencia(x, n - 1);					// Lineal, llamado n veces	== TOTAL O(n+1) = O(n)
 	}
 
-	public double potenciaPar(double x, int n) {
+	public double potenciaPar(double x, int n) {		//Complejidad
 		if (n == 1)
-			return x;
-		return potenciaPar(x * x, n / 2);
+			return x;									// 1
+		return potenciaPar(x * x, n / 2);				// Log[n] = TOTAL O(Log[n] + 1) = O(Log[n])
 	}
 
 
 	public double potenciaDinamica(double x, int n, double[] vPot) {
 		if (n == 0)
-			return 1;
+			return 1;									// 1
 		
-		if(vPot[n]==0)
-			vPot[n]=potencia(x, n - 1);
+		if(vPot[n]==0)									//
+			vPot[n]=potencia(x, n - 1);					// 	n
 		
-		return x * vPot[n];
+		return x * vPot[n];								// 1, TOTAL O(1+1+n) = O(n)
 	}
 
 	/*
